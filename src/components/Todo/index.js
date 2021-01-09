5e4b766fa2cf44120cc8f88332436f007157d324
Chukwuemeka Inya <chukwuemeka_andela@medable.com>
2021-01-09 13:38:35 +0100

    Add Proptypes to Todo component

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,28 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { FaTrashAlt, FaRegCircle } from "react-icons/fa";
 import { StyledLi } from './styles'
 import { removeTodo, completeTodo } from '../../actions/todos';
 import { TODO_STATUS } from '../../utils/constants'
 
-class Todo extends React.Component{
-    render(){
-        const { todo: { id, status, description }, onRemove, onComplete } = this.props;
+const Todo = props => {
+    const { todo: { id, status, description }, onRemove, onComplete } = props;
+        const isIncomplete = status === TODO_STATUS.INCOMPLETE
         return (
             <StyledLi status={status}>
                 {description}
                 <p>
-                    {status === TODO_STATUS.INCOMPLETE && <span onClick={() => onRemove(id)}><FaTrashAlt /></span>}
-                    <span onClick={() => onComplete(id)}><FaRegCircle /></span>
+                    {isIncomplete && <span onClick={() => onRemove(id)}><FaTrashAlt /></span>}
+                    <span onClick={isIncomplete ? () => onComplete(id) : () => {}}><FaRegCircle /></span>
                 </p>
             </StyledLi>
         )
-    }
 }
 
 const mapDispatchToProps = dispatch => ({
     onRemove: (id) => dispatch(removeTodo(id)),
     onComplete: (id) => dispatch(completeTodo(id))
 })
+
+Todo.propTypes = {
+    onRemove: PropTypes.func.isRequired,
+    onComplete: PropTypes.func.isRequired,
+    todo: PropTypes.shape({
+        id: PropTypes.string,
+        description: PropTypes.string,
+        startDate: PropTypes.instanceOf(Date),
+        endDate: PropTypes.instanceOf(Date),
+        status: PropTypes.string
+    }).isRequired
+}
 
 export default connect(null, mapDispatchToProps)(Todo);
